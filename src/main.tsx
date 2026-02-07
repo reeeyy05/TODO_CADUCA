dba1103df4060d9119fe0c3b6e2af48c89443025
Nicolas <152216316+nicolas09128@users.noreply.github.com>
2026-02-07 18:10:13 +0100

    ✅New: Cartas implementadas
    He implementado las cartas de la landing page y una de producto para los componentes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ProductsCards from './components/cards/ProductsCards.tsx'
+import LandingPageCards from './components/cards/LandingPageCards.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <LandingPageCards />
+    <ProductsCards />
   </StrictMode>,
 )
