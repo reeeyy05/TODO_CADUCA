9e480f68706bc3d2f9f567d088ad57aab506d906
Juanfrina <jfco11092024@outlook.com>
2026-02-08 17:59:32 +0100

    ✅new-componente-input-y-prueba

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import './index.css'
 import Button from './components/ui/Button';
 import Select from './components/ui/Select';
+import Input from './components/forms/Input';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <>
@@ -21,6 +23,18 @@ function App() {
             { value: 'opcion2', label: 'Opción 2' },
             { value: 'opcion3', label: 'Opción 3' },
           ]}
+        />
+      </div>
+
+      {/* Aquí puedes probar el Input */}
+      <div style={{ marginTop: '32px', background: '#171717', padding: '32px', maxWidth: '400px' }}>
+        <Input
+          label="Nombre"
+          name="nombre"
+          type="text"
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+          error={inputValue.length < 3 ? "El nombre debe tener al menos 3 caracteres" : undefined}
         />
       </div>
     </>
